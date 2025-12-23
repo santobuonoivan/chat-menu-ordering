08896c4342bdd5df801aeea7bd0b9612475fe416
meca <mecaluxivansantobuono@gmail.com>
2025-12-23 02:50:02 -0300

    feat: integrate session and menu stores in TopNavBar for dynamic restaurant title display

--- src/components/menuDigital/TopNavBar.tsx
+++ src/components/menuDigital/TopNavBar.tsx
@@ -3,6 +3,8 @@ import { RiRobot3Line } from "react-icons/ri";
 import { FaShoppingCart } from "react-icons/fa";
 import { useCartStore } from "@/stores/cartStore";
 import CartModal from "@/components/CartModal";
+import { useSessionStore } from "@/stores/sessionStore";
+import { useMenuStore } from "@/stores/menuStore";
 
 interface TopNavBarProps {
   onClose?: () => void;
@@ -13,7 +15,7 @@ export default function TopNavBar({ onClose }: TopNavBarProps) {
   const [totalItems, setTotalItems] = useState(0);
   const [showMenuPopover, setShowMenuPopover] = useState(false);
   const { getTotalItems, items } = useCartStore();
-
+  const { getSessionData } = useSessionStore();
   useEffect(() => {
     setTotalItems(getTotalItems());
   }, [getTotalItems, items]);
@@ -32,26 +34,16 @@ export default function TopNavBar({ onClose }: TopNavBarProps) {
   };
   return (
     <>
-      <header className="flex items-center justify-between whitespace-nowrap border-b border-slate-200/80 dark:border-slate-700/80 p-4 shrink-0">
-        <div className="flex items-center gap-3 text-text-light dark:text-text-dark">
-          <div className="size-6 text-primary">
+      <header className="flex items-center justify-between  whitespace-nowrap border-b border-slate-200/80 dark:border-slate-700/80 p-4 shrink-0">
+        <div className="flex items-center gap-3 text-text-light dark:text-text-dark w-[calc(100%-100px)]">
+          <div className="size-6 text-primary shrink-0">
             <RiRobot3Line size={24} color="#8E2653" />
           </div>
-          <h2 className="text-lg text-[#8E2653] font-bold tracking-tight">
-            Asistente AI{" "}
-            <span
-              style={{
-                fontFamily: "Comfortaa ,sans-serif",
-                fontWeight: "bold",
-                fontSize: 24,
-              }}
-            >
-              {" "}
-              appio
-            </span>
-          </h2>
+          <h4 className="text-md text-[#8E2653] font-bold tracking-tight truncate">
+            {/*Asistente AI*/} {getSessionData()?.rest.title || "Restaurante"}
+          </h4>
         </div>
-        <div className="flex items-center gap-3">
+        <div className="flex w-[100px] justify-end gap-3">
           <button
             className="relative flex items-center justify-center size-8 rounded-full hover:bg-slate-200/50 dark:hover:bg-slate-600/50 text-[#8E2653] dark:text-text-white transition-colors"
             onClick={handdleGoToDigitalMenu}
